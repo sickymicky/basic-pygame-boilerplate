8d7cf927701076ada983aa56cc31da37efeb439e
Mike Hall <mikehall40@live.co.uk>
2023-10-08 11:25:06 +0100

    ammend title

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pygame.init()
 screen_width = 600
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption(('Write Title Here'))
+pygame.display.set_caption(('Write Game Title Here'))
 
 run = True
 while run:
